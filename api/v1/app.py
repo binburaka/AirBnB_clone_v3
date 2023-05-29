14af4e87b7678eb056a6f93034e53375baaff9e4
obinna robinson <obinnarobinson1@gmail.com>
2023-05-30 00:50:37 +0300

    airbnb restful api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-"""RESTful API for Airbnb Clone"""
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
 from os import getenv
-from flask_cors import CORS
+
+
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def _storage(self):
-    """method to handle @app.teardown_appcontext that calls storage.close()"""
+def tear_down(self):
+    '''
+    close query after each session
+    '''
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """handler for 404 errors that returns a JSON-formatted 404
-    status code response."""
-    return jsonify({'error': 'Not found'}), 404
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-if __name__ == '__main__':
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=getenv('HBNB_API_PORT', '5000'), threaded=True)
+
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
